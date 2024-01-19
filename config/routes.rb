f0036166430bfbdbe00704e699196c12f61a718a
Alok Tantuway <34336422+alokaprince@users.noreply.github.com>
2024-01-19 19:09:32 +0530

    Update routes.rb (Logout issue)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :dealerbills
   resources :dealers
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   resources :works
   resources :dailyexps
   resources :stocks
